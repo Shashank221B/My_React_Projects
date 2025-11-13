713f4f4c677d258cd6d20b2acd01bc5d7b60098f
shashank chagi <shashankchagi.31@gmail.com>
2025-11-13 18:20:49 +0530

    Changes for responsiveness

--- F_SelectDropdown/selectdropdown/src/App.jsx
+++ F_SelectDropdown/selectdropdown/src/App.jsx
@@ -4,7 +4,7 @@ import Dropdown from './components/Dropdown'
 const App = () => {
   return (
     <div className='relative h-screen w-full flex  flex-col justify-center items-center'>
-      <h1 className='text-6xl font-bold text-blue-900 fixed top-10'>F_Select Dropdown</h1>
+      <h1 className='text-2xl sm:text-3xl md: text-4xl lg:text-6xl font-bold text-blue-900 fixed top-10'>F_Select Dropdown</h1>
       <Dropdown/>
     </div>
   )
