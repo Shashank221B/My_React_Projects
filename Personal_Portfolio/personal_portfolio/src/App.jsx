a982d45c680113fe2dd708d1073a7e46e09c01b3
shashank chagi <shashankchagi.31@gmail.com>
2025-11-17 09:31:05 +0530

    Changes made to Perosnal porfolio project

--- Personal_Portfolio/personal_portfolio/src/App.jsx
+++ Personal_Portfolio/personal_portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Skills from './sections/Skills'
